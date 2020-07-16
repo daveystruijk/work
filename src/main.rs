93a47a5731c5ad277c311537dce0f90ea77d311e
Davey Struijk <mail@daveystruijk.com>
2020-07-16 15:47:50 +0200

    Add navigation controls

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use chrono::{Datelike, DateTime, Timelike, Duration, Local};
 use std::{
     io::{self, stdout, Write},
     time::{self, Instant},
-    cmp::{max},
+    cmp::{min, max},
     fs::{File},
     path::{Path},
 };
 
 use crossterm::{
     cursor::{self, position},
-    event::{self, poll, read, Event, KeyCode, KeyEvent},
+    event::{self, poll, read, Event, KeyCode, KeyEvent, KeyModifiers},
     style::{self, SetForegroundColor, SetBackgroundColor, Color},
     terminal::{self, ClearType},
     execute,
@@ -70,26 +70,24 @@ use crossterm::{
 //   preparation time
 //
 
-//    let serialized = serde_json::to_string(&task).unwrap();
-//    println!("serialized = {}", serialized);
-//
-//    let deserialized: Task = serde_json::from_str(&serialized).unwrap();
-//    println!("deserialized = {:?}", deserialized);
-
 // TODO:
 // - Serialize to something more human-editable when format is more stable
 
-const APP_DATA_FILENAME: &str = "~/work.json";
+const APP_DATA_FILENAME: &str = "/Users/daveystruijk/work.json";
+
+const WORKING_ANIMATION_FRAMES: [char; 4] = ['▖', '▘', '▝', '▗'];
+
 
 enum Mode {
     Normal,
     Working,
-    Editing,
+    EditTask,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct AppData {
     tasks: Vec<Task>,
+
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -99,7 +97,7 @@ struct Task {
 }
 
 fn default_app_data() -> AppData {
-    let tasks = vec![
+    let tasks: Vec<Task> = vec![
         Task {
             text: String::from("Task 1"),
             minutes: 30,
@@ -109,22 +107,36 @@ fn default_app_data() -> AppData {
             minutes: 60,
         }
     ];
-    let app_data: AppData = {
-        tasks: tasks
-    };
+    let app_data: AppData = AppData { tasks };
     app_data
 }
 
 fn load_app_data() -> AppData {
 	let file = File::open(APP_DATA_FILENAME).unwrap();
     let deserialized: AppData = serde_json::from_reader(&file).unwrap();
-    println!("deserialized = {:?}", deserialized);
     deserialized
 }
 
 fn save_app_data(app_data: AppData) {
-    let serialized = serde_json::to_string(&app_data).unwrap();
-    println!("serialized = {}", serialized);
+	let file = File::create(APP_DATA_FILENAME).unwrap();
+    let serialized = serde_json::to_writer(&file, &app_data).unwrap();
+}
+
+pub fn read_line() -> Result<String> {
+    let mut line = String::new();
+    while let Event::Key(KeyEvent { code, .. }) = event::read()? {
+        match code {
+            KeyCode::Enter => {
+                break;
+            }
+            KeyCode::Char(c) => {
+                line.push(c);
+            }
+            _ => {}
+        }
+    }
+
+    return Ok(line);
 }
 
 fn render_current_task<W>(w: &mut W, task: &Task) -> Result<()>
@@ -136,7 +148,7 @@ where
     Ok(())
 }
 
-fn render_tasks<W>(w: &mut W, tasks: &Vec<Task>, selected_task_index: usize, start_time: &DateTime<Local>) -> Result<()>
+fn render_tasks<W>(w: &mut W, tasks: &Vec<Task>, selected_task_index: usize, start_time: &DateTime<Local>, animation_counter: usize, mode: &Mode) -> Result<()>
 where
     W: Write,
 {
@@ -152,20 +164,43 @@ where
         let duration_str = format!("{}", (start_of_day + duration).format("%-H:%M"));
         let target_str = format!("{}", head.format("%H:%M"));
 
-        if i == selected_task_index {
-            queue!(
-                w,
-                SetForegroundColor(Color::Yellow),
-                style::Print(">"),
-                style::Print(&text),
-            )?;
-        } else {
-            queue!(
-                w,
-                style::Print(" "),
-                style::Print(&text),
-            )?;
+        match mode {
+            Mode::Normal => {
+                if i == selected_task_index {
+                    queue!(
+                        w,
+                        SetForegroundColor(Color::Yellow),
+                        style::Print("• "),
+                        style::Print(&text),
+                    )?;
+                } else {
+                    queue!(
+                        w,
+                        style::Print("· "),
+                        style::Print(&text),
+                    )?;
+                }
+            }
+            Mode::Working => {
+                if i == 0 {
+                    queue!(
+                        w,
+                        SetForegroundColor(Color::Green),
+                        style::Print(WORKING_ANIMATION_FRAMES[animation_counter]),
+                        style::Print(&text),
+                    )?;
+                } else {
+                    queue!(
+                        w,
+                        style::Print("· "),
+                        style::Print(&text),
+                    )?;
+                }
+            }
+            Mode::EditTask => {
+            }
         }
+
         queue!(
             w,
             SetForegroundColor(Color::DarkGrey),
@@ -206,8 +241,8 @@ where
 
     let mode_str = match mode {
         Mode::Normal => "NORMAL",
-        Mode::Editing => "EDITING",
         Mode::Working => "WORKING",
+        Mode::EditTask => "EDITING",
     };
 
     queue!(
@@ -219,7 +254,7 @@ where
     Ok(())
 }
 
-fn render<W>(w: &mut W, tasks: &Vec<Task>, selected_task_index: usize, current_task_index: usize, mode: &Mode, start_time: &DateTime<Local>) -> Result<()>
+fn render<W>(w: &mut W, tasks: &Vec<Task>, selected_task_index: usize, current_task_index: usize, mode: &Mode, start_time: &DateTime<Local>, animation_counter: usize) -> Result<()>
 where
     W: Write,
 {
@@ -231,7 +266,7 @@ where
         cursor::MoveTo(0, 1)
     )?;
     render_current_task(w, &tasks[current_task_index])?;
-    render_tasks(w, &tasks, selected_task_index, start_time)?;
+    render_tasks(w, &tasks, selected_task_index, start_time, animation_counter, mode)?;
     render_timer(w)?;
     render_mode(w, mode)?;
     w.flush()?;
@@ -243,21 +278,23 @@ fn run<W>(w: &mut W) -> Result<()>
 where
     W: Write,
 {
+    // If no app data exists, create the file with default values
     if !Path::new(APP_DATA_FILENAME).exists() {
-
+        save_app_data(default_app_data());
     }
 
     let mut app_data: AppData = load_app_data();
 
-    let mode: Mode = Mode::Working;
+    let mut mode: Mode = Mode::Normal;
     let start_time = Local::now();
 
     let mut current_task_index: usize = 0;
     let mut selected_task_index: usize = 0;
+    let mut animation_counter: usize = 0;
 
     execute!(w, terminal::EnterAlternateScreen)?;
     terminal::enable_raw_mode()?;
-    render(w, &app_data.tasks, selected_task_index, current_task_index, &mode, &start_time)?;
+    render(w, &app_data.tasks, selected_task_index, current_task_index, &mode, &start_time, animation_counter)?;
 
     loop {
         // Wait up to 1s for another event
@@ -266,11 +303,18 @@ where
 
             match event {
                 Event::Key(key_event) => {
-                    if key_event.code == KeyCode::Esc {
+                    if key_event.modifiers == KeyModifiers::CONTROL && key_event.code == KeyCode::Char('c') {
+                        save_app_data(app_data);
                         break;
                     }
-                    if key_event.code == KeyCode::Char('j') {
-
+                    if key_event.code == KeyCode::Enter {
+                        match mode {
+                            Mode::Normal => { mode = Mode::Working }
+                            _ => { mode = Mode::Normal }
+                        }
+                    }
+                    if key_event.code == KeyCode::Esc {
+                        mode = Mode::Normal;
                     }
                     if key_event.code == KeyCode::Char('J') {
                         if selected_task_index + 1 < app_data.tasks.len() {
@@ -285,27 +329,54 @@ where
                         }
                     }
                     if key_event.code == KeyCode::Char('j') {
-                        selected_task_index = (selected_task_index + 1) % app_data.tasks.len();
-                    }
-                    if key_event.code == KeyCode::Char('k') {
-                        if selected_task_index == 0 {
-                            selected_task_index = app_data.tasks.len() - 1;
-                        } else {
-                            selected_task_index = selected_task_index - 1;
+                        match mode {
+                            Mode::Normal => {
+                                if selected_task_index + 1 < app_data.tasks.len() {
+                                    selected_task_index = selected_task_index + 1;
+                                }
+                            }
+                            _ => {}
                         }
                     }
-                    if key_event.code == KeyCode::Char('>') {
+                    if key_event.code == KeyCode::Char('k') {
+                        match mode {
+                            Mode::Normal => {
+                                if selected_task_index != 0 {
+                                    selected_task_index = selected_task_index - 1;
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    if key_event.code == KeyCode::Char('+') || key_event.code == KeyCode::Char('=') {
                         app_data.tasks[selected_task_index].minutes += 15;
                     }
-                    if key_event.code == KeyCode::Char('<') {
-                        app_data.tasks[selected_task_index].minutes = max(0, app_data.tasks[selected_task_index].minutes - 15);
+                    if key_event.code == KeyCode::Char('-') {
+                        app_data.tasks[selected_task_index].minutes = max(15, app_data.tasks[selected_task_index].minutes - 15);
+                    }
+                    if key_event.code == KeyCode::Char('n') {
+                        mode = Mode::EditTask;
+                        selected_task_index = app_data.tasks.len();
+                        app_data.tasks.push(Task {
+                            text: String::from(""),
+                            minutes: 15,
+                        });
+                    }
+                    if key_event.code == KeyCode::Char('e') {
+                        mode = Mode::EditTask;
+                        let input = read_line();
                     }
                 }
-                _ => {}
+                _ => {
+                    animation_counter += 1;
+                    if animation_counter >= WORKING_ANIMATION_FRAMES.len() {
+                        animation_counter = 0;
+                    }
+                }
             }
         }
 
-        render(w, &app_data.tasks, selected_task_index, current_task_index, &mode, &start_time)?;
+        render(w, &app_data.tasks, selected_task_index, current_task_index, &mode, &start_time, animation_counter)?;
     }
 
     // Cleanup
